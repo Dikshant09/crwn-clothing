abfe26461fea24ffbb475741a35e9493e43e2908
Dikshant <Dikshant1535.be20@chitkara.edu.in>
2022-03-25 16:54:43 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { BrowserRouter, Routes, Route, Link, } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h2>HatsPage</h2>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <Routes>
+      <Route path="/" element={<HomePage /> } />
+      <Route path="/hats" element={<HatsPage /> } />
+      </Routes>
     </div>
   );
 }
